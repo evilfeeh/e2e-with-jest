f995681ada7469bab548d3935375d0107ad10079
felipegsantos15 <felipegoncalvesdsantos@gmail.com>
2022-03-09 19:33:16 -0300

    test: create simple tests on jest

---
+++ test/e2e/api.test.js
@@ -0,0 +1,23 @@
+import { jest, describe, expect, test } from "@jest/globals"
+import superTest from 'supertest'
+import server from '../../src/server.js'
+
+describe('API TEST E2E', () => {
+    test('/GET - Should return an array', async () => {
+        const response = await superTest(server).get('/')
+        const data = JSON.parse(response.text)
+        expect(data).toBeInstanceOf(Array)
+        expect(data.length).toEqual(0)
+    })
+    test('/POST - should return an item and return ok', async ()=> {
+        const response = await superTest(server)
+        .post('/')
+        .send({
+            nome: 'teste testing',
+            age: 30
+        })
+        const expected = JSON.stringify({ok: 1})
+        expect(response.text).toStrictEqual(expected)
+    })
+    test.todo('/delete - test delete router')
+})
